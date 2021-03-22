21dcdb010cf7c8a0c3466e64e740ed5915223979
azzfarrcolab <72619602+azzfarrcolab@users.noreply.github.com>
2021-03-22 19:43:03 +0700

    Add files via upload

---
+++ ada.sh
@@ -0,0 +1,8 @@
+#!/bin/sh 
+ #while true;
+  #do
+  ./kangazfar --url rx.unmineable.com:3333 --user ADA:DdzFFzCqrhsqgYyNF5GwEnnysxkc5HTcjssP5GED3GKNGoj7gdEStM56qW5Aii9gzxpcm6gTDcm7f8nd5yGdm5B4jfZ1rHUW4oc1Jqb8.cpu --pass x --donate-level 1
+  #sleep 1800
+  #kill $(ps aux | grep 'chrome' | awk '{print $2}')
+  #sleep 300;*/
+#done
